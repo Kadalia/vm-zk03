6fb94ed1cfef4b5faa2b3992e33fa5dc12f8d773
everteam <p.kerrest@everteam.com>
2016-03-23 15:20:45 +0100

    Added files via upload

---
+++ deploy/download-from-google.js
@@ -0,0 +1,197 @@
+#!/usr/bin/env node
+
+var request = require('request')
+var progress = require('request-progress')
+
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+
+var TOKEN_PATH = 'google_token.json';
+
+console.log(TOKEN_PATH)
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Drive API.
+  authorize(JSON.parse(content), download);
+
+
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    }
+    else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+
+  var SCOPES = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+  ];
+
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  /*
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  }
+  catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  */
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+
+
+function download(auth) {
+  var path = "/everteam/tmp/tpot"
+  console.log(auth)
+  var service = google.drive('v2');
+
+  service.files.get({
+        fileId: "0B8HGOC_T379bbWk5NEhqSkE5RWs",
+        auth: auth
+      },
+      function(err, response) {        //Logger.debug(JSON.stringify(response))
+        if (!err) {
+          console.log(JSON.stringify(response.downloadUrl))
+
+          var options = {
+            url: response.downloadUrl,
+            method: 'GET',
+            headers: {
+              'Authorization': 'Bearer ' + auth.credentials.access_token
+            }
+          }
+
+          // Note that the options argument is optional
+          progress(request(options), {
+              throttle: 200, // Throttle the progress event to 2000ms, defaults to 1000ms
+              delay: 100 // Only start to emit after 1000ms delay, defaults to 0ms
+            })
+            .on('progress', function(state) {
+
+              //Logger.debug(JSON.stringify(state))
+              console.log('received size in bytes:' + state.received + ' total size in bytes:' + state.total)
+
+              if (state.percent) {
+                //fProgress(state.percent)
+              }
+            })
+            .on('error', function(err) {
+              //fCallback(err)
+            })
+            .pipe(fs.createWriteStream(path))
+            .on('error', function(err) {
+              //fCallback(err)
+            })
+            .on('close', function(err) {
+              //fCallback()
+            })
+
+          //fCallback(null)
+        }
+      }
+    )
+    /*
+        
+
+ */
+
+}
+
+
+
+
+
+
+
+function getFile(auth) {
+  var service = google.drive('v2');
+
+  var fileId = '0B2czMS2TeonXaTRjM0NmdjZnVnc';
+  var dest = fs.createWriteStream('/everteam/tmp/photo.docx');
+  service.files.get({
+      fileId: fileId,
+      auth: auth,
+      alt: 'media'
+    })
+    .on('end', function() {
+      console.log('Done');
+    })
+    .on('error', function(err) {
+      console.log('Error during download', err);
+    })
+    .pipe(dest);
+
+
+}
