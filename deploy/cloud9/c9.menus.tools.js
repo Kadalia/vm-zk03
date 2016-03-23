2880f370ee3bb6047f6e2759865da1004c1a1eae
everteam <p.kerrest@everteam.com>
2016-03-23 15:15:30 +0100

    Update c9.menus.tools.js

--- deploy/cloud9/c9.menus.tools.js
+++ deploy/cloud9/c9.menus.tools.js
@@ -13,3 +13,39 @@
             shell("./compact.sh\n");
         }
     }), 10, handle);
+    
+    menus.addItemByPath("Everteam/Deploy",  new ui.menu(), 90, handle);
+
+    menus.addItemByPath("Everteam/Deploy/et-webapp", new ui.item({ 
+        onclick: function(){ 
+            shell("./deploy/deploy.sh et-webapp 0B2czMS2TeonXSndnY2ZpUHoxVWM\n");
+        }
+    }), 10, handle);
+
+    menus.addItemByPath("Everteam/Deploy/et-solr", new ui.item({ 
+        onclick: function(){ 
+            shell("./deploy/deploy.sh et-solr 0B2czMS2TeonXMi1oaDlUZU00RDA\n");
+        }
+    }), 20, handle);
+
+    menus.addItemByPath("Everteam/Deploy/et-zookeeper", new ui.item({ 
+        onclick: function(){ 
+            shell("./deploy/deploy.sh et-zookeeper 0B2czMS2TeonXWlhVMEMzY1VKM0k\n");
+        }
+    }), 30, handle);
+
+    menus.addItemByPath("Everteam/Deploy/et-data", new ui.item({ 
+        onclick: function(){ 
+            shell("./deploy/deploy.sh et-data 0B2czMS2TeonXcmNfSWdudFBWejA\n");
+        }
+    }), 40, handle);
+
+    menus.addItemByPath("Everteam/Deploy/pg-webapp", new ui.item({ 
+        onclick: function(){ 
+            shell("./deploy/deploy.sh pg-webapp 0B2czMS2TeonXOS1IMnVnYVEydzQ\n");
+        }
+    }), 50, handle);
+                
+                
+                
+                  
