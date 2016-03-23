cca61e5df58295f36f8a7b07620eff869b285da7
everteam <p.kerrest@everteam.com>
2016-03-23 16:30:26 +0100

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,15 @@
-
 #!/bin/bash
 source ./deploy/tools.sh
 source ./env.sh
 
 deployModule=$1
 deployGoogleFileId=$2
+
+echo
+success "---------------------------------------------------------"
+info "Deploy $1 from Google ($2)"
+success "---------------------------------------------------------"
+echo
 
 ./deploy/download-from-google.js $deployModule $deployGoogleFileId
 ./deploy/$deployModule/deploy.sh $deployModule/$deployGoogleFileId
