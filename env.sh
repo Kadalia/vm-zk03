fdab0aa60a582651a4ae2601705ca205478fd26b
everteam <p.kerrest@everteam.com>
2016-03-24 14:34:12 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,47 +1,32 @@
 #!/bin/bash
 
-ubuntu=$(uname -v | grep "Ubuntu")
-debian=$(uname -v | grep "Debian")
-tinycore=$(uname -r | grep "3.16.6-tinycore")
+source ./os.sh
 
-if [ ! -z "$ubuntu" ]; then 
+if [ "$os" == "ubuntu" ]; then 
   workspaceDir=/everteam
   workspaceIsMounted=no
-  os="ubuntu"
   ip=`/sbin/ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'`
   psqlDir=/usr/lib/postgresql/9.4/bin
 fi
 
-if [ ! -z "$debian" ]; then 
+if [ "$os" == "debian" ]; then 
   workspaceDir=/everteam
   workspaceIsMounted=no
-  os="debian"
   ip=`/sbin/ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'`
   psqlDir=/usr/lib/postgresql/9.4/bin
 fi
 
 
-if [ ! -z "$tinycore" ]; then 
+if [ "$os" == "tinycore" ]; then 
   workspaceDir=/mnt/sda
   workspaceIsMounted=yes
-  os="tinycore"
   ip=`ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'`
 fi
 
-if [ -f /etc/redhat-release ]; then 
+if [ "$os" == "centos" ]; then 
   workspaceDir=/home/local/everteam
   workspaceIsMounted=no
-  os="centos"
   ip=`/sbin/ifconfig eth0 | awk '/inet /{print substr($2,1)}'`
   psqlDir=/usr/pgsql-9.4/bin
 fi
-
-
-
-if [ -z "$os" ]; then 
-	exit_failure "OS not supported !"
-fi
-
-
-
 
