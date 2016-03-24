47fc2778d931010f68ab5a56c7a6831710af4e57
everteam <p.kerrest@everteam.com>
2016-03-24 17:38:16 +0100

    Update init.sh

--- deploy/pg-data/init.sh
+++ deploy/pg-data/init.sh
@@ -21,7 +21,7 @@ cp ./deploy/pg-data/c9.menus.* /everteam/home/c9.menus/
 logFile="/everteam/tmp/initdb.log"
 info "initdb (see $logFile)"
 sudo chown postgres /everteam/home/pg-data
-sudo -u postgres $psqlDir/initdb -D /everteam/home/pg-data > $logFile 2>&1
+sudo sudo -u postgres $psqlDir/initdb -D /everteam/home/pg-data > $logFile 2>&1
 
 cp ./deploy/pg-data/start-postgres.sh /everteam/home/
 cp ./deploy/pg-data/stop-postgres.sh /everteam/home/
