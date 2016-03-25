127d895d55886a72492fd10ebe665c0203728a38
everteam <p.kerrest@everteam.com>
2016-03-25 11:01:07 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -26,7 +26,18 @@ fi
 if [ "$os" == "centos" ]; then 
   workspaceDir=/everteam
   workspaceIsMounted=no
-  ip=`/sbin/ifconfig eth0 | awk '/inet /{print substr($2,1)}'`
+  
+  v7=$(cat /etc/redhat-release | grep  -F " 7.")
+  v6=$(cat /etc/redhat-release | grep  -F " 6.")
+
+  if [! -z "$v7"]; then 
+    ip=`/sbin/ifconfig eth0 | awk '/inet /{print substr($2,1)}'`
+  fi
+  if [! -z "$v6"]; then 
+    ip=`ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'`
+  fi
+  
   psqlDir=/usr/pgsql-9.4/bin
 fi
+
 
