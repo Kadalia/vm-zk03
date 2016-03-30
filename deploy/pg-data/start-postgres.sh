06623a717b2e145948450b5176a60147821eba7d
everteam <p.kerrest@everteam.com>
2016-03-30 10:24:05 +0200

    Update start-postgres.sh

--- deploy/pg-data/start-postgres.sh
+++ deploy/pg-data/start-postgres.sh
@@ -3,14 +3,18 @@ source ./deploy/tools.sh
 source ./env.sh
 
 logFile="/everteam/tmp/startPostgres.log"
+logFilePostgres="/everteam/tmp/postgres.log"
+
 
 echo
 info "Start Postgres (see $logFile)"
 echo
 
 if [ -d "$(readlink -f /everteam/pg-data/)" ]; then 
-
-    sudo sudo -u postgres $psqlDir/pg_ctl start -D /everteam/pg-data/ -l $logFile > $logFile 2>&1
+    sudo rm -rf $logFilePostgres
+    sudo echo "..." > $logFilePostgres
+    sudo chmod 777 $logFilePostgres
+    sudo sudo -u postgres $psqlDir/pg_ctl start -D /everteam/pg-data/ -l $logFilePostgres > $logFile 2>&1
 
 else
 
