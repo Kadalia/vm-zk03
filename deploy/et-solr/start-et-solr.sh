bf98c89b433e6b6412841cc830936abc830d3120
everteam <p.kerrest@everteam.com>
2016-04-18 15:03:58 +0200

    Update start-et-solr.sh

--- deploy/et-solr/start-et-solr.sh
+++ deploy/et-solr/start-et-solr.sh
@@ -14,17 +14,19 @@ export JAVA_HOME=/everteam/java
 if [ -d "$(readlink -f /everteam/et-solr/)" ]; then 
 
     /everteam/et-zookeeper/bin/zkServer.sh start > $logFile 2>&1
+    sleep 20
     
     cd /everteam/et-solr/bin
+
+    if [ ! -f  /everteam/et-zookeeper/secured ]; then 
+        echo put security stufs to solr
+        ../server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:2181 -cmd putfile /security.json ../server/solr/security.json
+        ../server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:2181 -cmd putfile /solr.xml ../server/solr/solr.xml
+        echo secured > /everteam/et-zookeeper/secured
+    fi
     
-    ../server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:2181 -cmd putfile /security.json ../server/solr/security.json
-    ../server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:2181 -cmd putfile /solr.xml ../server/solr/solr.xml  
-    #../server/scripts/cloud-scripts/zkcli.sh -z localhost:2181 -cmd upconfig -d ../server/solr/configsets/et_testcorpus_config/conf -n ettestconf
 
     cd /everteam/et-solr
 
     ./bin/solr start -c -p 8983 -z $ip:2181 >> $logFile 2>&1
 fi
-
-    
-
