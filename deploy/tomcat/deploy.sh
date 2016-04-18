4e1315ce081a0c8bb7307d05533d4f2b62b7a3f2
everteam <p.kerrest@everteam.com>
2016-04-18 17:51:24 +0200

    Update deploy.sh

--- deploy/tomcat/deploy.sh
+++ deploy/tomcat/deploy.sh
@@ -2,32 +2,42 @@
 source ./deploy/tools.sh
 source ./env.sh
 
-info "Deploy Tomcat $1"
+info "Deploy Tomcat from tar $1"
 
-if [ -d "$(readlink -f /everteam/tomcat/)" ]; then 
-    rm -rf $(readlink -f /everteam/tomcat/)
+if [ ! -f "$1" ]; then 
+    error "Tomcat tar is not found"
+
+else
+
+    if [ -d "$(readlink -f /everteam/tomcat/)" ]; then 
+        rm -rf $(readlink -f /everteam/tomcat/)
+    fi
+    
+    if [ -f "/everteam/tomcat" ]; then 
+        rm -rf /everteam/tomcat
+    fi
+    
+    log=/everteam/tmp/log.txt
+    
+    cp ./deploy/tomcat/c9.menus.* /everteam/home/c9.menus
+    
+    cd /everteam/softs
+    
+    tar zxvf $1 > $log
+    rm -rf $1
+    
+    tomcat=$(sed -n '1p' $log | sed -e 's@/.*@@')
+    tomcatDir=/everteam/softs/$tomcat
+    
+    rm $log
+    
+    if [ -z "$tomcat" ] ; then 
+        error "Something is wrong"
+    else
+    
+        ln -s  $tomcatDir /everteam/tomcat
+        success "Tomcat deployed to $tomcatDir"
+    
+    fi
+
 fi
-
-if [ -f "/everteam/tomcat" ]; then 
-    rm -rf /everteam/tomcat
-fi
-
-log=/everteam/tmp/log.txt
-
-
-cp ./deploy/tomcat/c9.menus.* /everteam/home/c9.menus
-
-cd /everteam/softs
-
-tar zxvf $1 > $log
-rm -rf $1
-
-tomcat=$(sed -n '1p' $log | sed -e 's@/.*@@')
-tomcatDir=/everteam/softs/$tomcat
-
-rm $log
-
-ln -s  $tomcatDir /everteam/tomcat
-
-success "Tomcat deployed to $tomcatDir"
-
