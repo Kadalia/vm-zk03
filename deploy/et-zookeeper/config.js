98d3f3f3a958fa31de2d1e5592b22498bbbe1dc4
everteam <p.kerrest@everteam.com>
2016-04-18 14:36:56 +0200

    Update config.js

--- deploy/et-zookeeper/config.js
+++ deploy/et-zookeeper/config.js
@@ -7,40 +7,10 @@ var everteam = '/everteam/et-webapp'
 var libxmljs = require("libxmljs")
 
 if(! ip) {
-       console.log("usage : deploySolrConfig [ip]")
+       console.log("usage : config [ip]")
 } else {
 
-       var paramESCI = everteam + '/apps/esci/conf/param.xml'
-       
-       /*
-       console.log("Configuring everteam webapp")
-       console.log("paramCore file:" + paramESCI)
-       */
-       
-       var content = ""
 
-       var fs = require('fs')
-
-       fs.readFile(paramESCI, 'utf8', function (err,data) {
-              if (err) {
-                     return console.log(err);
-              }
-       
-              var xmlDoc = libxmljs.parseXml(data)
-
-
-              xmlDoc.get("//param[PARAMSEQ='esci.solr.secure']/CONTENT").text("true")
-              xmlDoc.get("//param[PARAMSEQ='esci.solr.secure']").attr({priority:"true"})
-
-              xmlDoc.get("//param[PARAMSEQ='esci.solr.home']/CONTENT").text("/everteam/home/everteam/solr")
-              xmlDoc.get("//param[PARAMSEQ='esci.solr.home']").attr({priority:"true"})
-              
-              fs.writeFile(paramESCI, xmlDoc.toString(), function (err) {
-                     if (err) return console.log(err);
-              })
-       })
-
-       
 }
 
 
