48c4c848523c590190fb88b16b26f120daf586c7
everteam <p.kerrest@everteam.com>
2016-04-18 17:46:00 +0200

    Update download.sh

--- deploy/tomcat/download.sh
+++ deploy/tomcat/download.sh
@@ -3,8 +3,8 @@ source ./deploy/tools.sh
 source ./env.sh
 
 logfile=/everteam/tmp/downloadTomcat.log
-info "Download Tomcat 7.0.68 (see $logfile)"
+info "Download Tomcat 7.0.69 (see $logfile)"
 
 cd /everteam/softs
-wget http://www.us.apache.org/dist/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz > $logfile 2>&1
-mv /everteam/softs/apache-tomcat-7.0.68.tar.gz $1
+wget http://www.us.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz > $logfile 2>&1
+mv /everteam/softs/apache-tomcat-7.0.69.tar.gz $1
